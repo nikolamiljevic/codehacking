f903d02e393d74549a435caaeb029f7e3bb3a46a
Nikola <milje18@gmail.com>
2018-12-22 21:36:52 +0100

    adding media page

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -28,7 +28,7 @@
 
 
 
-
+    @yield('styles')
 </head>
 
 <body id="admin-page">
@@ -178,11 +178,11 @@
                         <a href="#"><i class="fa fa-wrench fa-fw"></i>Media<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
-                                <a href="/media">All Media</a>
+                                <a href="{{route('media.index')}}">All Media</a>
                             </li>
 
                             <li>
-                                <a href="">Upload Media</a>
+                                <a href="{{route('media.create')}}">Upload Media</a>
                             </li>
 
                         </ul>
@@ -363,7 +363,8 @@
     <script src="{{ asset('js/sb-admin-2.js') }}"></script>
     <script src="{{ asset('js/scripts.js') }}"></script>
 
-@yield('footer')
+@yield('scripts')
+
 
 
 
