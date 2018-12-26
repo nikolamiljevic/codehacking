d3f43d024d02449724d9250a6e421ef1c4f866a4
Nikola <milje18@gmail.com>
2018-12-26 09:29:18 +0100

    deleting media done

--- app/Http/Controllers/AdminMediasController.php
+++ app/Http/Controllers/AdminMediasController.php
@@ -18,4 +18,21 @@ class AdminMediasController extends Controller
         
         return view('admin.media.create');
     }
+
+    public function store(Request $request){
+        $file = $request->file('file');
+        $name = time().$file->getClientOriginalName();
+        $file->move('images',$name);
+        Photo::create(['file'=>$name]);
+    }
+
+    public function destroy($id){
+
+        $photo = Photo::findOrFail($id);
+
+        unlink(public_path().$photo->file);
+        $photo->delete();
+
+        return redirect('admin/media');
+    }
 }
--- resources/views/admin/media/index.blade.php
+++ resources/views/admin/media/index.blade.php
@@ -22,7 +22,14 @@
                 <td>{{$photo->id}}</td>
                 <td><img height="50" src="{{$photo->file}}" alt=""></td>
                 <td>{{$photo->created_at ? $photo->created_at : "no date"}}</td>
-               
+               <td>
+                {!!Form::open(['method'=>'delete','action'=>['AdminMediasController@destroy',$photo->id]])!!}
+                       
+                <div class="form-group">
+                    {!!Form::submit('Delete media',['class'=>'btn btn-danger col-sm-4'])!!}
+                </div>
+                {!!Form::close()!!}
+               </td>
                 </tr>
             @endforeach
           
